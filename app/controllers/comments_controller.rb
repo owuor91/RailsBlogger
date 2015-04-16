a610b933d58d1fd31d4528845a8c3670329da93c
owuor91 <owuor91@gmail.com>
2015-04-16 16:57:57 +0300

    Completed app

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+	before_filter :require_login, except: [:create]
 	def create
 		@comment = Comment.new(author_name: params[:comment][:author_name], body: params[:comment][:body])
 		@comment.article_id = params[:article_id]
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+	resources :author_sessions, only: [:new, :create, :destroy]
+
+  get 'login' => 'author_sessions#new'
+  get 'logout' => 'author_sessions#destoy'	
+
   resources :articles do
   	resources :comments
   end
   resources :tags
+  resources :authors
+
   root to: 'articles#index'
 end
